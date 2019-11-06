6b58bad860e449d241655138fa836a09332c1b0a
Richard <nyjsarang@gmail.com>
2019-11-06 22:04:57 +0900

    Create Clean Architecture modules.

---
+++ boilerplate-application/src/main/kotlin/pe/richard/architecture/boilerplate/MainActivity.kt
@@ -0,0 +1,16 @@
+package pe.richard.architecture.boilerplate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pe.richard.architecture.core.Core
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Core.test()
+    }
+
+}
